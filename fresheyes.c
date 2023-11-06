c92b5f135a607482e8a6c1102b7bcea822756926
rachelie <rachelie.dwg@gmail.com>
2023-11-07 01:58:28 +0900

    write_final_output is fixed :)

--- fresheyes.c
+++ fresheyes.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define MAX_LENGTH 120
 
 // helper functions ============================================================
 // takes input
 void take_input(char** dna, int* string_num){
-    //FILE *input_file = fopen("hw2_input.txt", "r");
-    FILE *input_file = fopen("dna_sequences.txt", "r");
+    FILE *input_file = fopen("hw2_input.txt", "r");
+    //FILE *input_file = fopen("dna_sequences.txt", "r");
 
     //scan number of inputs
     fscanf(input_file, "%d", string_num); 
@@ -53,67 +54,54 @@ void write_final_results(char** dna, int size, char* lcs){
     FILE *output;
     output = fopen("hw2_output.txt", "w");
 
-    // append elements to aligned dna one by one according to the lcs alignment
     int aligned_index = 0;
-    int less_than_size = 1; //checks if indexes are less than size
-    while(less_than_size){    
+    while(indexes[0] < strlen(dna[0]) && indexes[1] < strlen(dna[1]) && indexes[2] < strlen(dna[2]) && indexes[3] < strlen(dna[3]) && indexes[4] < strlen(dna[4])){
         for(int i = 0; i < size; i++){
-            if(indexes[i] > strlen(dna[i])){ //if longer than the string, continue
-                less_than_size = 0;
-            } 
-            // if dna sequence is longer than lcs, just append the rest 
-            else if(lcs_index > strlen(lcs)){
+            if(dna[i][indexes[i]] != lcs[lcs_index]){
                 aligned_dna[i][aligned_index] = dna[i][indexes[i]];
-            }
-            //if current character is different from lcs, add it to aligned dna
-            else if(dna[i][indexes[i]] != lcs[lcs_index]){
-                aligned_dna[i][aligned_index] = dna[i][indexes[i]];
-            } 
-            // if current character is the same as lcs and is common, add it to aligned dna
-            else if(dna[i][indexes[i]] == lcs[lcs_index] && is_common(dna, size, lcs, lcs_index, indexes)){
+            }else if (lcs[lcs_index] == dna[i][indexes[i]] && is_common(dna, size, lcs, lcs_index, indexes)){ //aligned
                 aligned_dna[i][aligned_index] = dna[i][indexes[i]];
                 lcs_index++;
-            }
-            // if current character is the same as lcs, but just not in the right place, leave it as a dash
-            else{
-                indexes[i]--;
-            }
+            }else{
+                indexes[i]--; //if not aligned, leave as dash
+            } 
         }
         aligned_index++;
-        for(int i = 0; i< size; i++) indexes[i]++;
+        for(int i = 0; i <size; i++) indexes[i]++;
     }
 
+    //appending any left over characters after alignment
+    int longest_aligned_length = 0;
     for(int i = 0; i < size; i++){
-        if(indexes[i] > strlen(dna[i])) continue;
-        else if(indexes[i] < strlen(dna[i])){
+        if(indexes[i] < strlen(dna[i])){
+            int temp_aligned_index = aligned_index;
+            if((strlen(dna[i]) - indexes[i]) > longest_aligned_length) longest_aligned_length = strlen(dna[i]) - indexes[i];
             for(int j = indexes[i]; j < strlen(dna[i]); j++){
-                aligned_dna[i][aligned_index] = dna[i][j];
-                aligned_index++;
+                aligned_dna[i][temp_aligned_index] = dna[i][j];
+                temp_aligned_index++;
             }
         }
     }
+    longest_aligned_length += aligned_index;
 
-    //write aligned sequences to file
+    // write aligned sequences to file
     for(int i = 0; i < size; i++) {
-        for(int j = 0; j < aligned_index; j++){
-            if(aligned_dna[i][j] == '\0') fputc('-', output);
-            else fputc(aligned_dna[i][j], output);
-        }
+        for(int j = 0; j < longest_aligned_length; j++) fputc(aligned_dna[i][j], output);
         fputc('\n', output);
-        printf("%s\n", aligned_dna[i]);
     }
 
-    //print asterisks
-    int i = 0;
+    // print asterisks 
+    lcs_index = 0;
     for(int j = 0; j < aligned_index; j++){
-        if(i < strlen(lcs) && lcs[i] == aligned_dna[0][j]){
+        if(lcs_index > strlen(lcs)) break;
+        else if(lcs[lcs_index] == aligned_dna[0][j]){
             fputc('*', output);
-            i++;
+            lcs_index++;
         }else{
             fputc(' ', output);
         }
     }
-
+    
     fclose(output);
 }
 
@@ -166,16 +154,16 @@ char* find_lcs2(char** dna){
     while(i > 0 && j > 0){
         if(table[i-1][j] == table[i][j]) {
             i--;
-        }
-        else if (table[j-1][i] == table[i][j]) {
+        } else if (table[j-1][i] == table[i][j]) {
             j--;
-        }
-        else{
+        } else{
             lcs[lcs_index] = dna[0][i-1];
             i--; j--;
             lcs_index--;
         }
     }
+
+    free(table);
 
     return lcs;
 }
@@ -231,6 +219,8 @@ char* find_lcs3(char** dna){
             lcs_index--;
         }
     }
+
+    free(table);
 
     return lcs;
 }
@@ -292,6 +282,8 @@ char* find_lcs4(char** dna){
             i--; j--; k--; l--;
         }
     }
+    
+    free(table);
 
     return lcs;
 }
@@ -361,18 +353,25 @@ char* find_lcs5(char** dna){
         }
     }
 
+    free(table);
+
     return lcs;
 }
 
+char* find_lcs5_alternative(char** dna){
+    return NULL;
+}
+
 int main(){
+    //Checking run time
+    //at beginning of main
+    int TIME = 0;
+    clock_t start = clock();
+
     int string_num = 0;
     char* dna[5];
     take_input(dna, &string_num); //store the input sequences
     char* lcs;
-
-    for(int i = 0; i < string_num; i++){
-        printf("%s\n", dna[i]);
-    }
 
     if(string_num == 2){
         lcs = find_lcs2(dna);
@@ -381,9 +380,18 @@ int main(){
     }else if(string_num == 4){
         lcs = find_lcs4(dna);
     }else{
-        lcs = find_lcs5(dna);
+        if(get_max(strlen(dna[0]), strlen(dna[1]), strlen(dna[2]), strlen(dna[3]), strlen(dna[4])) < 80) lcs = find_lcs5(dna);
+        else lcs = find_lcs5_alternative(dna);
     }
     
     printf("lcs: %s\n", lcs);
     write_final_results(dna, string_num, lcs);
+
+    //free memory
+    free(lcs);
+    for(int i = 0; i < string_num; i++) free(dna[i]);
+
+    //at end of main
+    TIME += ((int)clock() - start) / (CLOCKS_PER_SEC / 1000);
+    printf("TIME : %d ms\n", TIME);
 }
